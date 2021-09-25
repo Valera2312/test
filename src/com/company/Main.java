8d9b4b930bb45fea00f92664aa3ff990f60697c2
Валерий Проскуряков <79242258075@yandex.ru>
2021-09-25 15:54:21 +1000

    fixed3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,13 +25,19 @@ public class Main {
            }
            Map<String, Integer> hashMap = new HashMap<>();
            String[] roman = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV",
-                   "XVI", "XVII", "XVIII", "XIX", "XX"};
+                   "XVI", "XVII", "XVIII", "XIX", "XX","XXI",
+                   	"XXII","	XXIII","XXIV","XXV","XXVI","XXVII","XXVIII","XXIX","XXX","XXXI","XXXII","XXXIII","XXXIV",
+                   "XXXV","	XXXVI","XXXVII","XXXVIII","XXXIX","XL","XLI","XLII","XLIII","XLIV","XLV","XLVI","XLVII","XLVIII",
+                   "XLIX","	L","LI","LII","	LIII","LIV","LV","LVI","LVII","LVIII","LIX","LX","LXI","LXII","LXIII",
+                   "LXIV","	LXV","LXVI","LXVII","LXVIII","LXIX","LXX","LXXI","LXXII","LXXIII","LXXIV","LXXV","LXXVI",
+                   "LXXVII","LXXVIII","LXXIX","LXXX","LXXXI","LXXXII","LXXXIII","LXXXIV","LXXXV","LXXXVI","LXXXVII",
+                   "LXXXVIII","	LXXXIX","XC","XCI","XCII","XCIII","XCIV","XCV","XCVI","	XCVII","XCVIII","XCIX","C"};
 
-           for (int i = 0; i <= 19; i++) {
+           for (int i = 0; i <= 99; i++) {
                hashMap.put(roman[i], i + 1);
            }
 
-
+           System.out.println(hashMap);
            String whatAreNumbers;
 
            if (expression_array[0].matches("[-+]?\\d+") && expression_array[2].matches("[-+]?\\d+")) {
